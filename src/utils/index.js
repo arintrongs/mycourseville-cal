b411d314deab558575551ae5da6bab24ef3c6da1
Arin Trongsantipong <arintrongs@gmail.com>
2019-04-23 12:34:29 +0700

    Fix Stupid

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
 export const withBackendUrl = path => {
-  if (process.env.NODE_ENV === "development") {
+  if (process.env.NODE_ENV === "production") {
     return `https://35.240.202.97${path}`
   } else {
     return `http://localhost:3003${path}`
