6a892ff3f45215ddb2f3718273eb0c3c3a406c3a
Arin Trongsantipong <arintrongs@gmail.com>
2019-04-23 12:30:53 +0700

    add back prod url

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 export const withBackendUrl = path => {
-  if (process.env.NODE_ENV === "production") {
-    return `https://mycoursevillecal.firebaseapp.com/api${path}`
+  if (process.env.NODE_ENV === "development") {
+    return `https://35.240.202.97${path}`
   } else {
     return `http://localhost:3003${path}`
   }
